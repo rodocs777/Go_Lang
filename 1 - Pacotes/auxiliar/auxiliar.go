8904c09fc41042e321e354e6c003077b83099698
Rodolfo <105673932+rodocs777@users.noreply.github.com>
2024-05-01 13:53:40 -0300

    Estudos com Go Lang

---
+++ 1 - Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,10 @@
+package auxiliar
+
+import "fmt"
+
+
+func Escrever () {
+	fmt.Println("Escrever do arquivo main")
+	escrever2()
+
+}
---
+++ 1 - Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+
+func escrever2 () {
+	fmt.Println("Escrever pela funcao escrever2")
+
+}
